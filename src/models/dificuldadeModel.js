80ee19556cf5036ab9bcd92552ba2df3f0a1f528
o guti <gustavo.millamonte10@gmail.com>
2026-05-18 12:10:20 -0300

    dificuldade model

--- src/models/dificuldadeModel.js
+++ src/models/dificuldadeModel.js
@@ -2,7 +2,7 @@ const pool = require('../config/database');
 
 async function listarTodos() {
   const result = await pool.query(
-    'SELECT * FROM questoes ORDER BY idc'
+    'SELECT * FROM dificuldade ORDER BY id_d'
   );
   return result.rows;
 }
@@ -11,53 +11,42 @@ async function buscarPorId(id) {
   // PostgreSQL usa $1, $2, $3... como placeholders
   // (SQLite usava ? ? ?)
   const result = await pool.query(
-    'SELECT * FROM questoes WHERE idc = $1',
+    'SELECT * FROM dificuldade WHERE id_d = $1',
     [id]
   );
   return result.rows[0];
-}
-
-async function buscarComLike(nome) {
-  const sql = 'SELECT enunciado, resposta FROM questoes WHERE enunciado ILIKE $1';
-  
-  const result = await pool.query(
-    sql,
-    [`%${nome}%`]
-  );
-  
-  return result.rows;
 }
 
 async function criar(dados) {
   const { topicoid, enunciado, resposta, link_bib, dtinclusao } = dados;
 
   const sql = `
-    INSERT INTO questoes (topicoid, enunciado, resposta, link_bib, dtinclusao)
-    VALUES ($1, $2, $3, $4, $5)
+    INSERT INTO dificuldade (grau)
+    VALUES ($1)
     RETURNING *
   `;
   
   const result = await pool.query(
     sql,
-    [topicoid, enunciado, resposta, link_bib, dtinclusao]
+    [grau]
   );
   
   return result.rows[0];
 }
 
 async function atualizar(id, dados) {
-  const { topicoid, enunciado, resposta, link_bib, dtinclusao } = dados;
+  const { grau } = dados;
   
   const sql = `
-    UPDATE questoes
-    SET topicoid = $1, enunciado = $2, resposta = $3, link_bib = $4, dtinclusao = $5
-    WHERE idc = $6
+    UPDATE dificuldade
+    SET grau = $1
+    WHERE id_d = $2
     RETURNING *
   `;
   
   const result = await pool.query(
     sql,
-    [topicoid, enunciado, resposta, link_bib, dtinclusao, id]
+    [grau, id_d]
   );
   
   return result.rows[0] || null;
@@ -65,7 +54,7 @@ async function atualizar(id, dados) {
 
 async function deletar(id) {
   const result = await pool.query(
-    'DELETE FROM questoes WHERE idc = $1',
+    'DELETE FROM dificuldade WHERE id_d = $1',
     [id]
   );
 
@@ -75,7 +64,6 @@ async function deletar(id) {
 module.exports = {
   listarTodos,
   buscarPorId,
-  buscarComLike,
   criar,
   atualizar,
   deletar
